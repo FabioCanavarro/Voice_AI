de8cc1d2886de6cc205d163cfbd2ccb4b5502a73
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-04-06 15:04:36 +0700

    things

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import speech_recognition as sr
+import os
 
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+try:
+    print(r.recognize_wit())
+except:
+    print("ERROR")
